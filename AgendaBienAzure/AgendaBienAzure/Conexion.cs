b4085c9a46b0a3420febe6222aadc6fe0016d3fc
aaron <aaron@DESKTOP-S8KEGS6>
2016-10-24 23:08:20 -0500

    Agregar archivos de proyecto.

---
+++ AgendaBienAzure/AgendaBienAzure/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaBienAzure
+{
+    public static class Conexion
+    {
+        public static string conexion = @"http://teshx.azurewebsites.net/";
+    }
+}
